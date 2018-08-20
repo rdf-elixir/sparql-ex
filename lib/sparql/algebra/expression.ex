90d78ea8ee75388071e2bc1390d16d5f9d076d80
Marcel Otto <marcelotto@gmx.de>
2018-08-20 02:22:41 +0200

    General evaluation of RDF terms

--- lib/sparql/algebra/expression.ex
+++ lib/sparql/algebra/expression.ex
@@ -5,3 +5,28 @@ defprotocol SPARQL.Algebra.Expression do
   def evaluate(expr, data)
 
 end
+
+defimpl SPARQL.Algebra.Expression, for: RDF.Literal do
+  def variables(_), do: []
+  def evaluate(literal, _), do: literal
+end
+
+defimpl SPARQL.Algebra.Expression, for: RDF.IRI do
+  def variables(_), do: []
+  def evaluate(iri, _), do: iri
+end
+
+defimpl SPARQL.Algebra.Expression, for: RDF.BlankNode do
+  def variables(_), do: []
+  def evaluate(bnode, _), do: bnode
+end
+
+defimpl SPARQL.Algebra.Expression, for: RDF.Literal do
+  def variables(_), do: []
+  def evaluate(literal, _), do: literal
+end
+
+defimpl SPARQL.Algebra.Expression, for: BitString do
+  def variables(_), do: []
+  def evaluate(variable, %{solution: solution}), do: Map.get(solution, variable)
+end
