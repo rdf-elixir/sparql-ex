f5e7c5132645c6611c1956dfa64797d3dfe98c56
Marcel Otto <marcelotto@gmx.de>
2019-06-09 21:48:34 +0200

    Include test support files via elixirc_paths

--- mix.exs
+++ mix.exs
@@ -13,6 +13,7 @@ defmodule SPARQL.Mixfile do
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env()),
 
       # Hex
       package: package(),
@@ -81,4 +82,6 @@ defmodule SPARQL.Mixfile do
     ]
   end
 
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
 end
